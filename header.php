f54f177f37f68020502edcb405424e8c2670386b
therrianj <therrian.j@gmail.com>
2020-12-04 19:41:06 +0800

    local bckgrnd fonts

--- header.php
+++ header.php
@@ -14,14 +14,14 @@
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom fonts for this template -->
-  <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+<!--   <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet"> -->
 
   <!-- Custom styles for this template -->
-  <link href="css/business-casual.min.css" rel="stylesheet">
+  <link href="css/business-casual.css" rel="stylesheet">
 
 </head>
-<h1 class="site-heading text-center text-white d-none d-lg-block"><img class='navbar-brand' height='150px' src='toastglow.png' alt=''/>
+<h1 class="site-heading text-center text-white d-none d-lg-block"><img class='navbar-brand' height='150px' src='img/toastglow.png' alt=''/>
     <span class="site-heading-upper text-primary mb-3">Golden Brown to perfection</span>
     <span class="site-heading-lower">TOASTpix.com</span>
   </h1>
@@ -29,7 +29,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
     <div class="container">
-      <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="index.php"><img class='navbar-brand' height='150px' src='toastglow.png' alt=''/>TOASTpix.com</a>
+      <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="index.php"><img class='navbar-brand' height='150px' src='img/toastglow.png' alt=''/>TOASTpix.com</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
--- toastdex.php
+++ toastdex.php
@@ -14,16 +14,12 @@
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom fonts for this template -->
-
-<!--   <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet"> -->
-
-  <link href="fonts/Lora:400,400i,700,700i" rel="stylesheet">
-  <link href="fonts/Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
 
   <!-- Custom styles for this template -->
-  <link href="css/business-casual.css" rel="stylesheet">
-  <link href="css/toast.css" rel="stylesheet">
+<!--   <link href="css/business-casual.min.css" rel="stylesheet">
+  <link href="css/toast.css" rel="stylesheet"> -->
 
 </head>
 
@@ -36,19 +32,15 @@ require 'header.php';
 
 
 ?>  
-  <section>
+
     <div class="container">
-      <div class="intro">
-        <div class="logo-wrapper">
-          <img class="img-fluid mb-3 mb-lg-0 rounded" src="img/toastpix2.svg" alt="">
+      <div class="row">
+        <div class="col-md-8 logo-wrapper">
+          <img class="img-fluid mb-3 mb-lg-0 rounded" src="toastpix2.svg" alt="">
         </div>
-      </div>
-    </div>
-  </section>
-  <section class="page-section clearfix">
-    <div class="container">
-      <div class="intro">
-        <div class="intro-text left-0 text-center bg-faded p-5 rounded">
+      
+
+        <div class="col-md-4 intro-text left-0 text-center bg-faded p-5 rounded">
           <h2 class="section-heading mb-4">
             <span class="section-heading-upper">Fresh PIX</span>
             <span class="section-heading-lower">Worth TOASTING</span>
@@ -58,26 +50,28 @@ require 'header.php';
           <div class="intro-button mx-auto">
             <a class="btn btn-primary btn-xl" href="#">View our past projects!</a>
           </div>
-        </div>
-      </div>
-    </div>
-  </section>
 
-  <section class="mt-10 page-section cta" style="clear:both">
+        </div>
+    <!--   <div class="intro"> -->
+        
+    <!--   </div> -->
+    </div>
+  </div>
+
     <div class="container">
-<!--       <div class="row">
-        <div class="col-xl-9 mx-auto"> -->
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
           <div class="cta-inner text-center rounded">
             <h2 class="section-heading mb-4">
               <span class="section-heading-upper">Our Promise</span>
               <span class="section-heading-lower">To You</span>
             </h2>
             <p class="mb-0">We get the job done. You have a job and we want to toast it. White, wheat, multi-grain, it doesn't matter to us. There isn't a slice of bread on the shelf we can't toast.</p>
-<!--           </div>
-        </div> -->
+          </div>
+        </div>
       </div>
     </div>
-  </section>
+
   
 
   <footer class="footer text-faded text-center py-5">
